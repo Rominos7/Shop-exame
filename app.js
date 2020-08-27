cebe5e0cf00476cfb1bb64f44c1e3bda1621c5cd
Rominos7 <Rominos7@gmail.com>
2020-08-27 20:15:56 +0300

    added sequelize

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 //Routes
 const ShopLink = require('./routes/ShopLink');
 
+//include sequelize
+const Sequelize = require('./helper/database');
+
+
 //MW
 app.set('view engine','ejs');
 app.set('views','views');
@@ -16,5 +20,12 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'static')));
 
 app.use(ShopLink);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+Sequelize.sync().then(connectionResult =>{
+    console.log('connRes =>'+connectionResult);
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err=> console.log(err));
+
+
 
